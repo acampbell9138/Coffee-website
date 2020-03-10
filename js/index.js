96cbe155c521c79e92caa92d0be8bf7a2e0f43af
acampbell9138 <60412435+acampbell9138@users.noreply.github.com>
2020-03-10 21:00:35 -0230

    javascript part 1

---
+++ js/index.js
@@ -0,0 +1,46 @@
+console.log("Hello, how are you?");
+console.log("It's a very cold day!");
+
+let courseTitle= "Intro to web development";
+let courseLength="8 weeks";
+
+console.log(courseTitle);
+console.log(courseLength);
+
+let firstName="Annagray";
+let lastName="Campbell";
+let age="29";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+let courseAttendees=["Anskey","Terri", "Kat","Annagray"];
+console.log(courseAttendees[0]);
+
+let mySkills=["socialMedia", "analytics","projectManagement"];
+mySkill[0] = "Facebook";
+mySkill[3]= "writing"; 
+
+let course = {
+    title: "Intro to data science",
+    duration: "8 weeks",
+    attendees: ["Anskey","Terri", "Kat","Annagray"]
+
+}
+
+console.log(course);
+console.log(course.title);
+
+let person = {
+    firstName: "Annagray",
+    lastName: "Campbell",
+    age: "29",
+    mySkills: ["socialMedia", "analytics","projectManagement"]
+};
+
+person.nickname = "Mickey";
+person.mySkills[3] = "writing";
+
+
+
